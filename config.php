0eac9a2e243d7d58a906e3ef41fee47e687640b1
ServandoT <gonzalez.agar.carlos@iescamas.es>
2024-10-04 09:17:06 +0200

    Añadido libros.sql

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
 
+// Para la conexion
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
