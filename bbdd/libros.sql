eb19ed51beb789ff4bfaef5395721dd12746dd4e
ServandoT <gonzalez.agar.carlos@iescamas.es>
2024-10-04 09:19:44 +0200

    Añadido tabla libros

--- bbdd/libros.sql
+++ bbdd/libros.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE libreria;
 
-USE libreria
+USE libreria;
+CREATE TABLE libros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT,
+    precio DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    ISBN VARCHAR(255) NOT NULL
+);
