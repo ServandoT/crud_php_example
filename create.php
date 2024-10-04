2ef68df18bf20b5eb3546bd943aa3cac269b48ca
ServandoT <gonzalez.agar.carlos@iescamas.es>
2024-10-04 09:40:59 +0200

    Cambiado config y create

--- create.php
+++ create.php
@@ -8,15 +8,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $descripcion = $_POST['descripcion'];
     $precio = $_POST['precio'];
     $stock = $_POST['stock'];
+    $autor = $_POST['autor'];
+    $ISBN = $_POST['ISBN'];
 
     try {
-        $sql = "INSERT INTO jabones (nombre, descripcion, precio, stock) VALUES (:nombre, :descripcion, :precio, :stock)";
+        $sql = "INSERT INTO libros (nombre, descripcion, precio, stock, autor, ISBN) VALUES (:nombre, :descripcion, :precio, :stock, :autor, :ISBN)";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute(['nombre' => $nombre, 'descripcion' => $descripcion, 'precio' => $precio, 'stock' => $stock]);
+        $stmt->execute(['nombre' => $nombre, 'descripcion' => $descripcion, 'precio' => $precio, 'stock' => $stock, 'autor' => $autor, 'ISBN' => $ISBN]);
 
-        $message = 'Jabón añadido con éxito!';
+        $message = 'Libro añadido con éxito!';
     } catch (PDOException $e) {
-        $message = 'Error al añadir el jabón: ' . $e->getMessage();
+        $message = 'Error al añadir el libro: ' . $e->getMessage();
     }
 }
 ?>
@@ -48,7 +50,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <label for="stock">Stock:</label>
     <input type="number" name="stock" id="stock" required>
     <br>
-    <input type="submit" value="Añadir Jabón">
+    <label for="autor">Autor:</label>
+    <input type="text" name="autor" id="autor" required>
+    <br>
+    <label for="ISBN">ISBN:</label>
+    <input type="text" name="ISBN" id="ISBN" required>
+    <input type="submit" value="Añadir Libro">
 </form>
 
 </body>
